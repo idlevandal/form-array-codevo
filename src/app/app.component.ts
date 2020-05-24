12025fe9a51a471918afa9b523e37a506a6a24e4
David Allen <davetvmusic@hotmail.com>
2020-05-24 21:23:51 +1000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'form-array-codev';
+
+  public myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+  
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      name: [''],
+      email: [''],
+      password: [''],
+      alternateEmails: this.fb.array([])
+    })
+  }
+
+  get alternateEmails() {
+    return this.myForm.get('alternateEmails') as FormArray;
+  }
+
+  addEmail(): void {
+    this.alternateEmails.push(this.fb.control(''))
+  }
+
+  public deleteAltEmail(index: number): void {
+    this.alternateEmails.removeAt(index);
+  }
 }
